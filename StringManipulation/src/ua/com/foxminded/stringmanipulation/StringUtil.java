04c974db37d64524ee2169225ef1dd367cfbce50
AndriyBodnar0 <bodnarkyzuna@gmail.com>
2021-09-03 15:51:01 +0300

    fsg

---
+++ StringManipulation/src/ua/com/foxminded/stringmanipulation/StringUtil.java
@@ -0,0 +1,35 @@
+package ua.com.foxminded.stringmanipulation;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class StringUtil {
+
+	public static void main(String[] args) {
+ 
+		System.out.println("Input phrase");
+
+		Scanner sourceone = new Scanner(System.in);
+		String vyvod = sourceone.nextLine();
+
+		String[] words = vyvod.split(" ");
+
+		List<String> words1 = new ArrayList<>();
+		// List<Collection> words1 =new ArrayList<>()
+		System.out.println("number of words" + " " + "-" + " " + words.length);
+
+		for (int i = 0; i < words.length; i++) {
+
+			System.out.print(words[i] + " " + "-" + " ");
+
+			char[] letters = words[i].toCharArray();
+			System.out.println(letters.length + " " + ";");
+
+		}
+
+		// System.out.println("Collection the words");
+
+	}
+
+}
